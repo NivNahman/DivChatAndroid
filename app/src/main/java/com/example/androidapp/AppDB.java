72549f8aa6ce1d6101e003381066b9429061ad47
DvirAmram <dvir.amram@live.biu.ac.il>
2022-06-10 15:39:24 +0300

    erev hagyfyihg

--- app/src/main/java/com/example/androidapp/AppDB.java
+++ app/src/main/java/com/example/androidapp/AppDB.java
@@ -1,11 +1,38 @@
 package com.example.androidapp;
 
+import android.content.Context;
+
+import androidx.annotation.NonNull;
 import androidx.room.Database;
+import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.room.migration.Migration;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 
 
-@Database(entities = {Chat.class}, version = 1)
+@Database(entities = {Chat.class, Message.class}, version = 2)
 public abstract class AppDB extends RoomDatabase{
 
     public abstract ChatDao chatDao();
+    public abstract MessageDao messageDao();
+    private static AppDB db;
+
+//    static Migration migration = new Migration(1,2) {
+//        @Override
+//        public void migrate(@NonNull SupportSQLiteDatabase database) {
+//            database.execSQL("");
+//        }
+//    };
+
+    static AppDB getDb(Context context){
+        if (db == null) {
+//            Synchronized (AppDB.class){
+                    db = Room.databaseBuilder(context.getApplicationContext(), AppDB.class, "DivDB")
+                            .allowMainThreadQueries()
+//                            .addMigrations(migration)
+                            .build();
+//        }
+        }
+        return db;
+    }
 }
--- app/src/main/java/com/example/androidapp/ChatScreen.java
+++ app/src/main/java/com/example/androidapp/ChatScreen.java
@@ -1,10 +1,17 @@
 package com.example.androidapp;
 
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 import androidx.room.Room;
 
 import com.example.androidapp.databinding.ActivityChatScreenBinding;
@@ -15,43 +22,72 @@ import java.util.List;
 
 public class ChatScreen extends AppCompatActivity {
 
+    List<Message> messages = new ArrayList<Message>();
     private ActivityChatScreenBinding binding;
+    private AppDB db;
+    private ChatDao chatDao;
+    private MessageDao messageDao;
+    private TextView name;
+    private View back;
+    private View send;
+    private ArrayAdapter<Message> adapter;
+    private EditText input;
+    private int id;
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         binding = ActivityChatScreenBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
-
 //        db= Room.databaseBuilder(getApplicationContext(), AppDB.class, "DivDB").allowMainThreadQueries().build();
 //        chatDao = db.chatDao();
-//        //contacts = postDao.index().get(0).getChats();
+        chatDao = AppDB.getDb(getBaseContext()).chatDao();
+        id = getIntent().getExtras().getInt("id");
+        name = findViewById(R.id.textName);
+        back = findViewById(R.id.imageBack);
+        back.setOnClickListener(v -> {
+            Intent intent = new Intent(this, ContactList.class);
+            startActivity(intent);
+        });
+        name.setText(""+id+"");
+       messageDao = AppDB.getDb(getBaseContext()).messageDao();
+
+        send = findViewById(R.id.layoutSend);
+        send.setOnClickListener(v -> {
+            input = findViewById(R.id.inputMessage);
+            Message message = new Message(input.getText().toString(),java.time.LocalDateTime.now().toString(),true);
+            message.setId(id);
+            messageDao.insert(message);
+            // messages.add(message);
+            onResume();
+            input.setText("");
+        });
+
 //
-//        FloatingActionButton addContactBtn = findViewById(R.id.addContactBtn);
-//        addContactBtn.setOnClickListener(view -> {
-//            Contact contact = new Contact("a","a","a","a");
-//            Chat newChat = new Chat("a");
-//            chatDao.insert(newChat);
-////            chats.clear();
-////            chats.addAll(chatDao.index());
-////            adapter.notifyDataSetChanged();
+        ListView rvMessages = findViewById(R.id.chatRecyclerView);
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, messages);
+        rvMessages.setAdapter(adapter);
+
+//        for (Message message : messages){
+//            if (message.getSent()){
+//
+//            }
+//        }
+
+//        send = findViewById(R.id.layoutSend);
+//        send.setOnClickListener(v -> {
+//            input = findViewById(R.id.inputMessage);
+//            Message message = new Message(input.getText().toString(),java.time.LocalDateTime.now().toString(),true);
+//            message.setId(id);
+//            messageDao.insert(message);
+//            // messages.add(message);
 //            onResume();
+//            input.setText("");
 //        });
-//
-//        ListView lvContacts = findViewById(R.id.ContactList);
-//        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, chats);
-//        lvContacts.setAdapter(adapter);
-//
-//
-//
-//        lvContacts.setOnItemLongClickListener((adapterView,view,i,l)->{
-//            Chat chat = chats.remove(i);
-//            chatDao.delete(chat);
-//            adapter.notifyDataSetChanged();
-//            return true;
-//        });
+
+
 
 
 
@@ -60,10 +96,13 @@ public class ChatScreen extends AppCompatActivity {
 
     }
 
-//    @Override
-//    protected void onResume(){
-//
-//    }
+    @Override
+    protected void onResume(){
+        super.onResume();
+        messages.clear();
+        messages.addAll(messageDao.get(id));
+        adapter.notifyDataSetChanged();
+    }
 
 
 }
